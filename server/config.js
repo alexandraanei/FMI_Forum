32b9c682e6e1c0ea3fd6cb5b675b2b16113864c9
unknown <anei.alexandra98@gmail.com>
2020-05-19 03:21:05 +0300

    Improve backend, refactor code

---
+++ server/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoUri: "mongodb+srv://Alexandra:fmiforum@mycluster-p1t0g.gcp.mongodb.net/fmi-forum?retryWrites=true&w=majority"
+}
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const config = require('./config');
+
+mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => console.log('Connected to MongoDB'));
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/auth', require("./controllers/Auth"));
+app.use('/api/category', require("./controllers/Category"));
+app.use('/api/forum', require("./controllers/Forum"));
+app.use('/api/thread', require("./controllers/Thread"));
+app.use('/api/post', require("./controllers/Post"));
+
+const PORT = 5005;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
